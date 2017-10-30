fea8badd0a35fcd276afffc922efea4820b47738
Shajan Jacob <shajanjp@gmail.com>
2017-10-30 17:04:08 +0530

    adds users model

--- app/users/models/users.server.model.js
+++ app/users/models/users.server.model.js
@@ -1,8 +1,45 @@
-var mongoose = require('mongoose'),
-Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-	id : Schema.ObjectId
+	id : Schema.ObjectId,
+	first_name: String,
+	last_name: String,
+	username: String, 
+	password: String,
+	email: String
 });
 
-mongoose.model('user', userSchema);
+const User = module.exports = mongoose.model('user', userSchema);
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+	const query = { username: username }
+	User.findOne(query, callback);
+}
+
+module.exports.getUserByEmail = function(emil, callback){
+	const query = { email: email }
+	User.findOne(query, callback);
+}
+
+module.exports.addUser = function(newUser, callback){
+	bcrypt.genSalt(10, (err, salt) => {
+		bcrypt.hash(newUser.password, salt, (err, hash) => {
+			if(err) throw err;
+			newUser.password = hash;
+			newUser.save(callback);
+		});
+	});
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+		if(err) throw err;
+		callback(null, isMatch);
+	});
+}
