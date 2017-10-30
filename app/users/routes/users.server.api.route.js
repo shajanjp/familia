718860623641671c5c4066c9b0f74e4b210053c0
Shajan Jacob <shajanjp@gmail.com>
2017-10-30 17:04:21 +0530

    adds user API routes

--- app/users/routes/users.server.api.route.js
+++ app/users/routes/users.server.api.route.js
@@ -3,8 +3,21 @@ var domainRoot = '/api';
 
 module.exports = function(app){
 
-	app.route(domainRoot + '/users')
+	app.route(domainRoot + '/auth')
 	.get(usersController.home)
+	.post(usersController.authenticate);
+
+	app.route(domainRoot + '/signin')
+	.post(usersController.authenticate)
+
+	app.route(domainRoot + '/signup')
+	.post(usersController.create);
+
+	app.route(domainRoot + '/signout')
+	.get(usersController.signout);
+
+	app.route(domainRoot + '/users')
+	.get(usersController.authorize, usersController.listUsersAPI)
 	.post(usersController.home);
 
 	app.param('user_id', usersController.userById);
