7590c5cd669d7503577e0a7b34e48fd2c33adf3a
Shajan Jacob <shajanjp@gmail.com>
2018-01-18 12:21:29 +0530

    adds auth in home page

--- app/home/routes/home.server.route.js
+++ app/home/routes/home.server.route.js
@@ -1,7 +1,8 @@
 var homeController = require('../controllers/home.server.controller.js');
 var contactsController = require('../../contacts/controllers/contacts.server.controller.js');
+var usersController = require('../../users/controllers/users.server.controller.js');
 
 module.exports = function(app){
 	app.route('/')
-	.get(contactsController.contactsDashboardUI);
+	.get(usersController.authorize('ui'), contactsController.contactsDashboardUI);
 }
